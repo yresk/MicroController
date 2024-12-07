1614d160e79d22f99e450b6896c19078d5d4dee6
Martin Sander <ms@ebx.de>
2024-12-07 16:33:40 +0100

    x

--- RaspberryPico/MicroPython/Examples_ssd1306.py
+++ RaspberryPico/MicroPython/Examples_ssd1306.py
@@ -8,19 +8,20 @@ i2c = SoftI2C(scl=Pin(5), sda=Pin(4))
 
 oled_width = 128
 oled_height = 64
-display = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+display1 = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=0x3c)
+display2 = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=0x3d)
 
-display.fill(0)                         # fill entire screen with colour=0
-display.pixel(0, 10)                    # get pixel at x=0, y=10
-display.pixel(0, 10, 1)                 # set pixel at x=0, y=10 to colour=1
-display.hline(0, 8, 4, 1)               # draw horizontal line x=0, y=8, width=4, colour=1
-display.vline(0, 8, 4, 1)               # draw vertical line x=0, y=8, height=4, colour=1
-display.line(0, 0, 127, 63, 1)          # draw a line from 0,0 to 127,63
-display.rect(10, 10, 107, 43, 1)        # draw a rectangle outline 10,10 to 117,53, colour=1
-display.fill_rect(10, 10, 107, 43, 1)   # draw a solid rectangle 10,10 to 117,53, colour=1
-display.text('Hello World', 30, 0, 1)    # draw some text at x=0, y=0, colour=1
+display1.fill(0)                         # fill entire screen with colour=0
+display1.pixel(0, 10)                    # get pixel at x=0, y=10
+display1.pixel(0, 10, 1)                 # set pixel at x=0, y=10 to colour=1
+display1.hline(0, 8, 4, 1)               # draw horizontal line x=0, y=8, width=4, colour=1
+display1.vline(0, 8, 4, 1)               # draw vertical line x=0, y=8, height=4, colour=1
+display1.line(0, 0, 127, 63, 1)          # draw a line from 0,0 to 127,63
+display1.rect(10, 10, 107, 43, 1)        # draw a rectangle outline 10,10 to 117,53, colour=1
+display1.fill_rect(10, 10, 107, 43, 1)   # draw a solid rectangle 10,10 to 117,53, colour=1
+display1.text('Hello World', 30, 0, 1)    # draw some text at x=0, y=0, colour=1
 
-if True:
+if False:
     display.scroll(20, 0)                   # scroll 20 pixels to the right
     display.fill_rect(0,0,19,63,0)
 
@@ -28,21 +29,23 @@ if True:
 import framebuf
 fbuf = framebuf.FrameBuffer(bytearray(8 * 8 * 1), 8, 8, framebuf.MONO_VLSB)
 fbuf.line(0, 0, 7, 7, 1)
-#display.blit(fbuf, 10, 10, 0)           # draw on top at x=10, y=10, key=0
-display.show()
+#display1.blit(fbuf, 10, 10, 0)           # draw on top at x=10, y=10, key=0
+display1.show()
 
-display.contrast(255)
+display1.contrast(255)
  
-if False:
-    display.fill(0)
-    display.fill_rect(0, 0, 32, 32, 1)
-    display.fill_rect(2, 2, 28, 28, 0)
-    display.vline(9, 8, 22, 1)
-    display.vline(16, 2, 22, 1)
-    display.vline(23, 8, 22, 1)
-    display.fill_rect(26, 24, 2, 4, 1)
-    display.text('MicroPython', 40, 0, 1)
-    display.text('SSD1306', 40, 12, 1)
-    display.text('OLED 128x64', 40, 24, 1)
-    display.show()
+if True:
+    display2.fill(0)
+    display2.fill_rect(0, 0, 32, 32, 1)
+    display2.fill_rect(2, 2, 28, 28, 0)
+    display2.vline(9, 8, 22, 1)
+    display2.vline(16, 2, 22, 1)
+    display2.vline(23, 8, 22, 1)
+    display2.fill_rect(26, 24, 2, 4, 1)
+    display2.text('MicroPython', 40, 0, 1)
+    display2.text('SSD1306', 40, 12, 1)
+    display2.text('OLED 128x64', 40, 24, 1)
+    display2.show()
+
+
 
